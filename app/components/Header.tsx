acdd3497049d92b762c29e227601c86dbc2b6d05
Selin Karabıçak <selinss62@gmail.com>
2024-08-01 14:59:41 +0300

    Header updated

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,28 +3,28 @@ import Image from 'next/image';
 
 const Header: React.FC = () => {
     return (
-        <header className="bg-white flex flex-col pt-5">
-            <div className="flex justify-between">
-                <div className="flex items-center mb-3 ml-8 w-1/5">
+        <header className="bg-white flex flex-col">
+            <div className="flex justify-between pt-2">
+                <div className="flex items-center pb-1 ml-8 w-1/5">
                     <Image
-                        src="/hepsiburada_logo1.png"
+                        src="/logo.png"
                         alt="Hepsiburada Logo"
-                        width={150}
+                        width={60}
                         height={40}
                     />
                 </div>
-                <div className="flex mr-6 mb-6 font">
+                <div className="flex mr-6 mt-4 font">
                     <span className="font-bold font-sans text-xl">Link</span>
                     <span className="mr-2 font-sans text-xl">VOTE</span>
                     <span className="font-sans text-xl"> Challenge</span>
                 </div>
             </div>
             <div className="flex">
-                <div className="header-line bg-orange-500"></div>
-                <div className="header-line bg-blue-400"></div>
-                <div className="header-line bg-purple-500"></div>
-                <div className="header-line bg-green-500"></div>
-                <div className="header-line bg-purple-900"></div>
+                <div className="header-line bg-black"></div>
+                <div className="header-line bg-black"></div>
+                <div className="header-line bg-black"></div>
+                <div className="header-line bg-black"></div>
+                <div className="header-line bg-black"></div>
             </div>
         </header>
     );
